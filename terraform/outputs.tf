562810248a9fd078c6963088d837733b05d0d3d1
Mohammed Saad <msaad@mail.bradley.edu>
2025-08-20 02:18:13 -0400

    Fix: remove duplicate ec2_public_ip output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -20,11 +20,6 @@ output "alb_dns" {
 value = aws_lb.app_alb.dns_name
 }
 
-output "ec2_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
-}
-
 output "app_url" {
   value = "http://${aws_instance.app_server.public_ip}"
 }
