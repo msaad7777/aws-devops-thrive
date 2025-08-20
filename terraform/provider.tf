5dca25df4dfa7565b50819e0ff00e22d0865eeb4
Mohammed Saad <msaad@mail.bradley.edu>
2025-08-19 22:39:31 -0400

    Pin AWS provider to ~> 5.0 to match VPC module 5.1.0

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+
 provider "aws" {
   region = var.aws_region
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
