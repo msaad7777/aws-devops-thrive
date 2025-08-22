41452785073851c441653f3beca8c64a7e60a191
Mohammed Saad <msaad@mail.bradley.edu>
2025-08-21 20:58:46 -0400

    syntax

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -5,10 +5,25 @@ data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
 
-  filter { name = "name"                values = ["al2023-ami-*-kernel-6.1-*"] }
-  filter { name = "architecture"        values = ["x86_64"] }
-  filter { name = "virtualization-type" values = ["hvm"] }
-  filter { name = "root-device-type"    values = ["ebs"] }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-6.1-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 }
 
 ########################################
